e13411d78e6168f6f8928f1ebcc9b43bd242faa6
anviiiijain <anvijain250102@gmail.com>
2020-06-19 15:25:11 +0530

    loading perfect

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var x = document.getElementById("togglebtn").children;
 var w = window.matchMedia("(max-width=650px)");
 
 function loadPage() {
-    var t = setTimeout(loadHome, 8000);
+    var t = setTimeout(loadHome, 3950);
     console.log("loadpage works");
 }
 
